10472891dff34259bd6ed7a0e5a4ec87f79d74e1
ezgaro <ezgaro2005@gmail.com>
2025-02-01 12:28:28 +0200

    Adding real time search with URL query params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,7 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
   const { data: posts } = await sanityFetch({ query: STARTUP_QUERY });
   return (
     <>
